56928e7378bbc2eab96c47573c5bec02c5c4da9d
sunkuangdong <sunkuangdong@edianzu.com>
2022-01-26 19:03:26 +0800

    爬虫

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const request = require("request")
 const cheerio = require("cheerio")
+const cors = require("cors");
 const app = express()
-const port = 3000
+const port = 3001
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     request("http://www.jikexueyuan.com", function (error, response, body) {
         if (!error && response.statusCode === 200) {
             $ = cheerio.load(body) // 相当于 jQuery，获取服务器的 DOM
             res.json({
-                "Classnum": $(".aside-cList li").toString(),
+                "Classnum": $(".header ul").toString(),
             })
         }
     })
